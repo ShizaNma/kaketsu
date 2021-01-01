bbabd47eca56f40021f8c3ff66fefc0b4a3f82fa
ShizaNma <76829080+ShizaNma@users.noreply.github.com>
2021-01-01 15:48:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ var ctx = canvas.getContext("2d");
 var x = 175;
 var y = 0;
 var dx = 2;
-var dy = 4;
+var dy = 8;
 
 function draw() {
 var tofu = new Image();
 tofu.src = "tofu.png";
 tofu.onload = function() {
+ctx.clearRect(0, 0, canvas.width, canvas.height);
 ctx.drawImage(tofu, x, y);
 y += dy;
 };
