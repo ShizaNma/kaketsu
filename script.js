a28c94e53a7cd9cf99c828746b2721c0fc1bfd9f
ShizaNma <76829080+ShizaNma@users.noreply.github.com>
2021-01-01 14:29:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 function draw() {
 var canvas = document.getElementById('game');
-if (canvas.getContext) {
 var ctx = canvas.getContext('2d');
 ctx.fillRect(25, 25, 100, 100);
 ctx.clearRect(45, 45, 60, 60);
 ctx.strokeRect(50, 50, 50, 50);
 }
-}
-
