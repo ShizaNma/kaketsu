d82c02465d53f53af201dc99469e0c1d61c0fe2a
ShizaNma <76829080+ShizaNma@users.noreply.github.com>
2021-01-01 18:41:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ width: 450, height: 800, backgroundColor: 0x000000,
 });
 let el = document.getElementById('app');
 el.appendChild(app.view);
+let tofutxtr= new PIXI.Texture.from('tofu.png');
+let tofusprt = new PIXI.Sprite(tofutxtr);
+tofusprt.anchor.x = 0.5;
+tofusprt.anchor.y = 0.5;
+tofusprt.x = app.screen.width / 2;
+tofusprt.y = app.screen.height / 2;
+app.stage.addChild(tofusprt);
