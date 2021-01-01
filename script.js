912d00c93b1e2abb5e5d7ef2e196b3cda73f7827
ShizaNma <76829080+ShizaNma@users.noreply.github.com>
2021-01-01 14:15:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+(function(){"use strict";
+var gamecanvas = document.getElementById('game');
+var ctx = gamecanvas.getContext('2d');
 function draw() {
-const gamecanvas = document.getElementById('game');
-const ctx = gamecanvas.getContext('2d');
 ctx.fillStyle = "#ffffff";
-ctx.fillRect(440,200,20,20);
+ctx.fillRect(440, 200, 20, 20);
 }
+})();
