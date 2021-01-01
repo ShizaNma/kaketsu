ebd86f51e16acb7f04551a86cfc6cb97881d03a3
ShizaNma <76829080+ShizaNma@users.noreply.github.com>
2021-01-01 22:30:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
-let app = new PIXI.Application({
-width: 450, height: 800, backgroundColor: 0x000000, 
-});
-let el = document.getElementById('app');
-el.appendChild(app.view);
-
-PIXI.loader.add("tofu.png");
+const app = new PIXI.Application({ width: 450, height: 800 }); 
+document.body.appendChild(app.view); 
+app.renderer.view.style.position = "relative"; 
+app.renderer.view.style.width = "450px"; 
+app.renderer.view.style.height = "800px"; 
+app.renderer.view.style.display = "block"; 
+app.renderer.backgroundColor = 0x000000; 
+PIXI.loader.add("tofu.png")
 PIXI.loader.load((loader, resources) =>
 {
-let tofudy = 8;
-let gameLoops = [];
-function addGameLoop(gameLoopFunction)
-{
-app.ticker.add(gameLoopFunction);
-gameLoops.push(gameLoopFunction);
-}
-
-let tofu= new PIXI.Sprite(resources["tofu.png"].texture);
-tofu.anchor.x = 0.5;
-tofu.anchor.y = 0.5;
-tofu.x = app.screen.width / 2;
-tofu.y = 50;
-});
-app.stage.addChild(tofu);
-
-function gameLoop()
-{
-tofu.y += tofudy;
-if(tofu.y < 50 || tofu.y > 750)
-{
-tofudy = -tofudy;
-}
-}
-addGameLoop(gameLoop);
+    let gameLoops = []; 
+    let tofuVy = 8; 
+    function addGameLoop(gameLoopFunction)
+    {
+        app.ticker.add(gameLoopFunction); 
+        gameLoops.push(gameLoopFunction); 
+    }
+    function removeAllGameLoops()
+    {
+        for (const gameLoop of gameLoops)
+        {
+            app.ticker.remove(gameLoop);
+        }
+    function createGameScene()
+    {
+        removeAllScene(); 
+        removeAllGameLoops(); 
+        
