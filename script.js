abafad4da1dad1bac0f2708c5bd4ae76fd5e25c0
ShizaNma <76829080+ShizaNma@users.noreply.github.com>
2021-01-01 15:09:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,9 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
 ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
+ctx.rect(175, 0, 100, 100);
 ctx.fillStyle = "#FF0000";
 ctx.fill();
 ctx.closePath();
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+ctx.drawImage(tofu.png, 175, 0)
