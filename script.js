1957ffaf1917c76ae597fcd451fda35a182b07da
ShizaNma <76829080+ShizaNma@users.noreply.github.com>
2021-01-01 15:30:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(175, 0, 100, 100);
-ctx.strokeStyle = "#FF0000";
-ctx.stroke();
-ctx.closePath();
-
 var tofu = new Image();
 tofu.src = "tofu.png";
-tofu.onload = fanction() {
+tofu.onload = function() {
 ctx.drawImage(tofu, 175, 0);
 };
