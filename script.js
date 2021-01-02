f99578181c95f177ed4e02451546e7c87d4a3c87
ShizaNma <76829080+ShizaNma@users.noreply.github.com>
2021-01-02 09:44:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const app = new PIXI.Application({ width: 450, height: 800 }); 
 document.body.appendChild(app.view); 
 app.renderer.view.style.position = "relative"; 
-app.renderer.view.style.left = "50%"; 
 app.renderer.view.style.width = "450px"; 
 app.renderer.view.style.height = "800px"; 
 app.renderer.view.style.display = "block"; 
 app.renderer.backgroundColor = 0x000000; 
+app.renderer.view.style.transform = "translateX(−50%)"; 
+app.renderer.view.style.left = "50%"; 
 PIXI.loader.add("tofu.png")
 PIXI.loader.load((loader, resources) =>
 {
